0dc2e1654b40999e24cbdce1e56a7b2eb1f2992a
Monice Morais <monice.s.morais@gmail.com>
2021-12-14 15:02:59 -0300

    initial commit (flutter com web API - aula 3.2)

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:bytebank/http/webclient.dart';
+import 'package:bytebank/screens/dashboard.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BytebankApp());
+  findAll().then((transactions) => print('new transactions $transactions'));
+}
+
+class BytebankApp extends StatelessWidget {
+  const BytebankApp({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+      home: Dashboard(),
+    );
+  }
+}
