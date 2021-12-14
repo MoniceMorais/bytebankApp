ce77181136737d164e58a597c2332c33b32399e7
Monice Morais <monice.s.morais@gmail.com>
2021-12-14 15:12:17 -0300

    Flutter com web API - aula 3.4

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank/components/progress.dart';
 import 'package:bytebank/database/dao/contact_dao.dart';
 import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contact_form.dart';
@@ -27,17 +28,7 @@ class _ContactsListState extends State<ContactsList> {
             case ConnectionState.none:
               break;
             case ConnectionState.waiting:
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    CircularProgressIndicator(),
-                    Text('Loading')
-                  ],
-                ),
-              );
-              break;
+              return Progress();
             case ConnectionState.active:
               break;
             case ConnectionState.done:
@@ -49,7 +40,6 @@ class _ContactsListState extends State<ContactsList> {
                 },
                 itemCount: contacts!.length,
               );
-              break;
           }
           return Text('Unknown error');
         },
