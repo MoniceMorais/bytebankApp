77f11ac3c9ae22b7dae7e8bccecc465f6c74e13c
Monice Morais <monice.s.morais@gmail.com>
2021-12-16 20:15:07 -0300

    Flutter com web API - aula 5.6

--- lib/http/webclients/transaction_webclient.dart
+++ lib/http/webclients/transaction_webclient.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
-
 import 'package:bytebank/http/webclient.dart';
-import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/models/transaction.dart';
 import 'package:http/http.dart';
 
@@ -11,33 +9,21 @@ class TransactionWebclient {
     final url = Uri.parse(baseUrl);
     final Response response = await client.get(url).timeout(const Duration(seconds: 5));
 
-    return _toTransactions(response);
+    final List<dynamic> decodedJson = jsonDecode(response.body);
+    return decodedJson
+        .map((dynamic json) => Transaction.fromJson(json))
+        .toList();
   }
 
   Future<Transaction> save(Transaction transaction) async {
     final String transactionJson = jsonEncode(transaction.toJson());
     final url = Uri.parse(baseUrl);
-    final Response response = await client.post(url, headers: {
-      'Content-type': 'application/json',
-      'password': '1000',
-    }, body: transactionJson);
-
-    return _toTransaction(response);
+    final Response response = await client.post(url,
+        headers: {
+          'Content-type': 'application/json',
+          'password': '1000',
+        },
+        body: transactionJson);
+    return Transaction.fromJson(jsonDecode(response.body));
   }
-
-  List<Transaction> _toTransactions(Response response) {
-
-    final List<dynamic> decodedJson = jsonDecode(response.body);
-    final List<Transaction> transactions = [];
-    for (Map<String, dynamic> transactionJson in decodedJson) {
-      transactions.add(Transaction.fromJson(transactionJson));
-    }
-    return transactions;
-  }
-
-  Transaction _toTransaction(Response response) {
-    Map<String, dynamic> json = jsonDecode(response.body);
-    return Transaction.fromJson(json);
-  }
-  
 }
